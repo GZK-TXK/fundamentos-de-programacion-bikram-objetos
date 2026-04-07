3d98b6a51ae5fe05344fd100483448478dcb9b84
GZK-TXK <chocarrovaz.gaizka@gmail.com>
2026-04-07 13:09:47 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,105 @@
+const Coche = {
+  Marca: "Opel",
+  Modelo: "Astra",
+  Matrícula: "1460 SGA",
+  };
+
+const Casa = {
+  CodPostal: 15794,
+  Calle: "Desengaño 27",
+  Portal: 27,
+  Piso: "2º B",
+  };
+
+const FullStackDeveloper = {
+  lenguajes: ["Javascript", "Python", "Java", "PHP"],
+  proyectos: ["Calculadora web", "E-commerce", "Blog personal"]
+    };
+
+const perro = {
+  Nombre: "Harri",
+  Raza: "Dachshund",
+  Color: "Negro",
+  Edad: "6",
+  ladrar: function() {
+    console.log("Woof!");
+    },
+  popo: function() {
+    return Math.random() * 3;
+    }
+  }
+
+const marcaPortatil = Portatil.marca;
+
+const marcaPortatil2 = Portatil["marca"];
+
+const grupos = concierto.grupos;
+
+const RGB = [led.rojo, ler.verde, led.blue];
+
+Portatil.modelo = "GF-62 8RD";
+
+concierto.cartelera.push("Amelie Lens");
+
+concierto.fecha = new Date();
+
+Impresora.imprimiendo = {
+  NombreArchivo: "Imprimiendo PDF",
+  Copias: 1,
+  NumPaginas: 1
+  };
+
+const Noticia = {
+  titular: "Alonso gana su tercer mundial de pilotos.",
+  cuerpo: "Soñar es gratis pero hay que despertarse en algun momento asi que nada a seguir llorando."
+  };
+
+const Persona = {
+  Nombre: "Jokin",
+  Apellidos: "Diaz",
+  Edad: "56"
+  };
+
+const Avion = {
+  NumeroPasajeros: 250,
+  despegar: function(){
+    console.log("Despegando");
+    },
+  volar: function(){
+    console.log("Llegando al destino");
+    },
+  aterrizar: function(){
+    console.log("Aterrizando");
+    }
+  };
+
+const Paquete = {
+  contenido: ["ropa", "cepillo de dientes", "champú", "portatil", "cargador")
+              }
+const Pais = {
+  Numerohabitantes: 5600000000,
+  Continente: "Asia",
+  Pais: "India"
+    }
+const CodError = 0_Error.codigo;
+
+const Integrantes = Grupo.integrantes;
+
+const NivelesTinta = Impresora.tinta;
+
+const pixeles = Pantalla.pixeles;
+
+const especificaciones = Movil["especificaciones"];
+
+Grupo.numIntegrantes = 5;
+
+Pantalla.dimensiones = "1920x1080";
+
+if (led.encendido) {
+  led.encendido = false;
+  } else {
+  led.encendico = true;
+  };
+
+movil.temperatura = "35ª"
+  
